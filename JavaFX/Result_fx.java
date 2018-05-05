2776ba05becde28677bc3aeadedb38fbf1e3015c
kochi <kochi.nakajima@gmail.com>
2018-05-05 13:01:26 -0600

    IM SO SLEEPY

--- JavaFX/Result_fx.java
+++ JavaFX/Result_fx.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import javafx.scene.control.ScrollPane;
+
+
 import javafx.scene.layout.BorderPane;
 
 public class Result_fx extends BorderPane implements PaneState{
+	
+	private ArrayList<Integer> results;
+	
+	private ApplicationUI frame;
+
+	private ScrollPane sp;
+	
+	public void addComponents(ApplicationUI _frame, ArrayList<Integer> _results) {
+		results = _results;
+		sp.setContent(this);
+		frame = _frame;
+	}
 
 	public void addComponents() {
 		
